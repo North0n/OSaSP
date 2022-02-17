b945d4ce7d4dda7bf05e08d69d95f724c2f16f43
Ivan Mautsin <mautin.ivan.3@gmail.com>
2022-02-17 17:27:28 +0300

    Added parameters check task 7

--- laba1/task_7.sh
+++ laba1/task_7.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+C_FILE="$1"
+EXE_FILE="$2"
 
-if gcc "$1" -o "$2"
-then
-	./"$2"
+if [ $# -eq 2 ]; then
+	if [ -f "$C_FILE" ]; then
+		if gcc "$1" -o "$2"; then
+			./"$2"
+		fi
+	else
+		echo "File $C_FILE doesn't exist"
+	fi
+else
+	echo "You should enter 2 parameters"
+	echo "	First - C code file"
+	echo "	Second - name for executable file"
 fi
