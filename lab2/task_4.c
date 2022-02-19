0f89e48e3d8842e35cee0a43d793abb0b5676c7b
North0n <mautin.ivan.3@gmail.com>
2022-02-19 22:31:19 +0300

    Lab2 task 4 is done

---
+++ lab2/task_4.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int printStr(FILE *inputFile, FILE *outputFile)
+{
+    int ch = getc(inputFile);
+    while (ch != EOF && ch != '\n') {
+        if (putc(ch, outputFile) == EOF)
+            return EOF;
+        ch = getc(inputFile);
+    }
+    if (ch == '\n' && putc(ch, outputFile) == EOF)
+        return EOF;
+    return ch;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("You should enter 2 parameters:\n");
+        printf("    First - filename\n");
+        printf("    Second - group size\n");
+        return 1;
+    }
+
+    const char *filename = argv[1];
+    int groupSize = atoi(argv[2]);
+    if (groupSize < 0) {
+        printf("You entered wrong group size. It should be >=0\n");
+        return 1;
+    }
+    FILE *file;
+    if ((file = fopen(filename, "r")) == NULL) {
+        printf("Error during file opening attempt\n");
+        return 1;
+    }
+
+    int eof = 42;
+    int counter;
+    while (eof != EOF) {
+        counter = 0;
+        do {
+            eof = printStr(file, stdout);
+        } while (eof != EOF && ++counter != groupSize);
+        if (eof != EOF) {
+            printf("Press any key\n");
+            while (getchar() != '\n');
+        }
+    }
+    
+
+    if (fclose(file) == EOF) {
+        printf("Error during file closing attempt. May cause loss of data.\n");
+        return 1;
+    }
+
+    return 0;
+}
