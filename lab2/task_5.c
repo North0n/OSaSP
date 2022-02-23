75eb40ab29e83d937cc303950543c084f9926cd9
North0n <mautin.ivan.3@gmail.com>
2022-02-23 18:19:17 +0300

    Lab2 Task 5 Added error ouput to stderr

--- lab2/task_5.c
+++ lab2/task_5.c
@@ -6,12 +6,12 @@ int closeFiles(FILE *inpFile, FILE *outFile)
 {
     int error = 0;
     if (fclose(inpFile) == EOF) {
-        printf("Error during attempt to close first file. May cause loss of data.\n");
-        error = 1;
+        perror("Error during attempt to close first file. May cause loss of data.\n");
+        error = -3;
     }
     if (fclose(outFile) == EOF) {
-        printf("Error during attepmt to close second file. May cause loss of data.\n");
-        error = 1;
+        perror("Error during attepmt to close second file. May cause loss of data.\n");
+        error = -3;
     }
     return error;
 }
@@ -19,40 +19,40 @@ int closeFiles(FILE *inpFile, FILE *outFile)
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
-        printf("You should enter 2 parameters:\n");
-        printf("    First - input file's name\n");
-        printf("    Second - output file's name\n");
-        return 1;
+        fprintf(stderr, "You should enter 2 parameters:\n");
+        fprintf(stderr, "    First - input file's name\n");
+        fprintf(stderr, "    Second - output file's name\n");
+        return -1;
     }
 
     const char *inputFileName = argv[1];
     const char *outputFileName = argv[2];
     FILE *inputFile, *outputFile;
     if ((inputFile = fopen(inputFileName, "r")) == NULL) {
-        printf("Error during first file opening attempt\n");
-        return 1;
+        perror("Error during first file opening attempt\n");
+        return -2;
     }
     if ((outputFile = fopen(outputFileName, "w")) == NULL) {
-        printf("Error during second file opening attempt\n");
-        return 1;
+        perror("Error during second file opening attempt\n");
+        return -2;
     }
 
     struct stat accessRights;
     if (stat(inputFileName, &accessRights)) {
-        printf("Error during attempt to access information about first file\n");
+        perror("Error during attempt to access information about first file\n");
         closeFiles(inputFile, outputFile);
-        return 1;
+        return -4;
     }
     if (chmod(outputFileName, accessRights.st_mode)) {
-        printf("Error during attempt to give access rights of first file to the second file\n");
+        perror("Error during attempt to give access rights of first file to the second file\n");
         closeFiles(inputFile, outputFile);
-        return 1;
+        return -4;
     }
 
     char ch;
     while ((ch = getc(inputFile)) != EOF) {
         if (putc(ch, outputFile) == EOF) {
-            printf("Error during attempt to write to the second file\n");
+            perror("Error during attempt to write to the second file\n");   
             break;
         }
     }
