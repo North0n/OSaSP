6c09d4d06e063d6df372a46b4a89e1da48e715f0
North0n <mautin.ivan.3@gmail.com>
2022-02-23 18:21:59 +0300

    Lab2 Task 6 Added error ouput to stderr

--- lab2/task_6.c
+++ lab2/task_6.c
@@ -6,16 +6,16 @@ int printDir(const char *dirname)
     DIR *dir;
     printf("Contents of %s:\n", dirname);
     if ((dir = opendir(dirname)) == NULL) {
-        printf("Error during attempt to open directory %s\n", dirname);
-        return 1;
+        perror("Error during attempt to open directory.\n");
+        return -1;
     }
     struct dirent *dirent;
     while ((dirent = readdir(dir)) != NULL) {
         printf("    %s\n", dirent->d_name);
     }
     if (closedir(dir)) {
-        printf("Error during attempt to close directory %s\n", dirname);
-        return 1;
+        perror("Error during attempt to close directory.\n");
+        return -2;
     }
 
     return 0;
