e850956e4c29d36af62fbebb9e6acf93116f26cc
North0n <mautin.ivan.3@gmail.com>
2022-04-12 20:35:52 +0300

    Lab3 Individual Task is reworked with no signals

--- lab3/individual.c
+++ lab3/individual.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <limits.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-#define ERR_ARGS_COUNT (-1)
-#define ERR_FILE_OPEN (-2)
-#define ERR_FILE_CLOSE (-3)
-#define ERR_OPEN_DIR (-4)
-#define ERR_CLOSE_DIR (-5)
-#define ERR_SIG_ASSIGN (-6)
-#define ERR_MAX_PROC (-7)
+#define ERR_FILE_OPEN   (-2)
+#define ERR_FILE_CLOSE  (-3)
+#define ERR_DIR_OPEN    (-4)
+#define ERR_DIR_CLOSE   (-5)
+#define ERR_WAIT        (-6)
+#define ERR_ARGS_COUNT  (-1)
+#define ERR_ARGS_NUM    (-7)
 
-int processesCount = 1;
-
-void processExited(int x)
-{
-    while (waitpid(-1, NULL, WNOHANG) > 0) {
-        --processesCount;
-    }
-}
+long processesCount = 1;
+long maxProcessesCount = 0;
 
 int findFreq(const char *filename, const char *substr, int *lettersCount)
 {
@@ -59,89 +53,100 @@ int findFreq(const char *filename, const char *substr, int *lettersCount)
     return frequency;
 }
 
-int dirTraversal(char *dirname, const char *str, int maxProc)
+int dirTraversal(char *filename, const char *substr)
 {
     DIR *dir;
-    if ((dir = opendir(dirname)) == NULL) {
+    if ((dir = opendir(filename)) == NULL) {
         perror("Error during attempt to open directory.\n");
-        return ERR_OPEN_DIR;
+        return ERR_DIR_OPEN;
     }
-    struct dirent *dirent;
-    struct stat stats;
-    int length = (int)strlen(dirname);
+
+    struct dirent* dirEntry;
     int error = 0;
-    int frequency = 0;
-    int lettersCount = 0;
-    while ((dirent = readdir(dir)) != NULL) {
-        if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0)
+    struct stat stats;
+    int frequency, bytesCount;
+    int length = (int)strlen(filename);
+
+    while ((dirEntry = readdir(dir)) != NULL) {
+        // Skip current and previous directory
+        if (strcmp(dirEntry->d_name, ".") == 0 || strcmp(dirEntry->d_name, "..") == 0)
             continue;
-        dirname[length] = '/';
-        dirname[length + 1] = '\0';
-        strcat(dirname, dirent->d_name);
-        if (lstat(dirname, &stats)) {
-            fprintf(stderr, "Filename: %s\n", dirname);
+
+        // Form current file filename
+        filename[length] = '/';
+        filename[length + 1] = '\0';
+        strcat(filename, dirEntry->d_name);
+        if (lstat(filename, &stats) != 0) {
             perror("Error during attempt to get info about file.\n");
+            fprintf(stderr, "Filename: %s\n", filename);
             continue;
         }
-        if (S_ISDIR(stats.st_mode)) {
-            error |= dirTraversal(dirname, str, maxProc);
-        } else if (S_ISREG(stats.st_mode)) {
-            while (processesCount >= maxProc) {
-                processExited(0);
+
+        // Current file processing
+        if (S_ISDIR(stats.st_mode))
+            error |= dirTraversal(filename, substr);
+        else if (S_ISREG(stats.st_mode)) {
+            // Ensures that process count is less than or equal to maximum process count
+            if (processesCount >= maxProcessesCount) {
+                if (wait(NULL) == -1) {
+                    perror("Error during attempt to wait process termination\n");
+                    --processesCount;
+                    return ERR_WAIT;
+                }
+                --processesCount;
             }
-            pid_t pid = fork();
-            switch (pid) {
+            // Creation of a new process for file handling
+            switch (fork()) {
                 case -1:
                     perror("Error during attempt to start another process\n");
-                    break;
+                    continue;
                 case 0:
-                    frequency = findFreq(dirname, str, &lettersCount);
-                    if (frequency != ERR_FILE_CLOSE && frequency != ERR_FILE_OPEN) {
+                    frequency = findFreq(filename, substr, &bytesCount);
+                    if (frequency != ERR_FILE_OPEN && frequency != ERR_FILE_CLOSE) {
 //                        if (frequency != 0)
                         {
                             printf("Process's PID: %5d Full path: %100s Bytes count: %6d Matched words count: %6d\n",
-                                   getpid(), dirname, lettersCount, frequency);
+                                   getpid(), filename, bytesCount, frequency);
                         }
                     }
-                    _exit(0);
+                    exit(0);
                 default:
                     ++processesCount;
-                    break;
             }
         }
     }
-    if (closedir(dir)) {
+    if (closedir(dir) != 0) {
         perror("Error during attempt to close directory.\n");
-        return ERR_CLOSE_DIR;
+        return ERR_DIR_CLOSE;
     }
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     if (argc != 4) {
-        printf("You should enter 3 parameters:\n");
-        printf("    1. Directory's name\n");
-        printf("    2. String to find\n");
-        printf("    3. Maximum number of processes\n");
+        printf("You should enter 3 parameters:\n"
+                      "    1. Directory's name\n"
+                      "    2. String to find\n"
+                      "    3. Maximum number of processes\n");
         return ERR_ARGS_COUNT;
     }
 
-    int maxProc = (int)strtol(argv[3], NULL, 10);
-    if (maxProc <= 0 || maxProc == LONG_MAX) {
-        printf("Maximum number of processes must be more than 0\n");
-        return ERR_MAX_PROC;
-    }
-
-    if (signal(SIGCHLD, processExited) == SIG_ERR) {
-        perror("Error during attempt to assign signal to a process termination\n");
-        return ERR_SIG_ASSIGN;
+    maxProcessesCount = strtol(argv[3], NULL, 10);
+    if (maxProcessesCount <= 1 || errno == ERANGE) {
+        printf("Maximum number of processes must be more than 1 and less than LONG_MAX\n");
+        return ERR_ARGS_NUM;
     }
 
     char *dirname = (char*)malloc(PATH_MAX * sizeof(char));
     strcpy(dirname, argv[1]);
 
-    int errorCode = dirTraversal(dirname, argv[2], maxProc);
+    int errorCode = dirTraversal(dirname, argv[2]);
 
-    _exit(errorCode);
+    while (processesCount > 1) {
+        if (wait(NULL) == -1)
+            perror("Error during attempt to wait process termination\n");
+        --processesCount;
+    }
+
+    exit(errorCode);
 }
